7a2bb9e098069868f52df23e9b464c9442e13546
Lorna Brown <lornarbrown@gmail.com>
2015-10-17 21:03:00 -0600

    repair connect

--- modules/connect.php
+++ modules/connect.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 $dbHost = ''; 
 $dbPort = ''; 
 $dbUser = ''; 
@@ -10,14 +12,10 @@ $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
 $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT'); 
 $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME'); 
 $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+$dbName = 'shoe_inventory';
 
-// echo to check the connection
-		echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser";
+echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser";
 
 $db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-?>
-
-<?php // echo to check the connection
-		echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br/>\n";
 
 ?>
