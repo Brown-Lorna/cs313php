7b8080d9df04900a87138d6b26032ec701dac7f1
Lorna Brown <lornarbrown@gmail.com>
2015-10-17 23:30:46 -0600

    show try2

--- pages/shoe_inventory_main.php
+++ pages/shoe_inventory_main.php
@@ -4,21 +4,6 @@ require 'connect.php';
 
 $db = dbConnect();
 
-	// Get the inventory
-	$query = 'SELECT * FROM shoe_category
-				ORDER BY category_ID';
-	$categories = $db->query($query); 
-echo $categories;
- /*	$col_query = 'SELECT * FROM color
-				ORDER BY color_ID';
- $categories = $db->query($col_query);
-	$siz_query = 'SELECT * FROM size
-				ORDER BY size_ID';
- $categories = $db->query($siz_query);
-	$style_query = 'SELECT * FROM shoe_style
-				ORDER BY style_ID';
- $categories = $db->query($style_query);
-*/
 ?>
 <!DOCTYPE html>
 <html>
@@ -49,7 +34,16 @@ echo $categories;
 	-->	
 
 <p>Show available shoe categories<br />
-	<?php	echo $categories; 
+	<?php
+function dbConnect() {
+	// Get the inventory
+	$query = 'SELECT * FROM shoe_category
+				ORDER BY category_ID';
+	$categories = $db->query($query); 
+	return $categories;
+}
+	
+echo $categories; 
 		print $categories; 
 		?>
 		</p>
---
+++ pages/show_inventory.php
@@ -0,0 +1,23 @@
+<?php
+
+require 'connect.php';
+
+$db = dbConnect();
+
+?>
+
+ /*	// Get the inventory
+	$query = 'SELECT * FROM shoe_category
+				ORDER BY category_ID';
+	$categories = $db->query($query); 
+echo $categories;
+	$col_query = 'SELECT * FROM color
+				ORDER BY color_ID';
+ $categories = $db->query($col_query);
+	$siz_query = 'SELECT * FROM size
+				ORDER BY size_ID';
+ $categories = $db->query($siz_query);
+	$style_query = 'SELECT * FROM shoe_style
+				ORDER BY style_ID';
+ $categories = $db->query($style_query);
+*/
